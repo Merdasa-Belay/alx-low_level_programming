edf9520ff4d6c7e9e7d8ad49d57d6ea81fb8d468
Merdasa-Belay <merdasabelay@gmail.com>
2022-06-03 00:02:37 +0300

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 int main(void)
 {
-char name[100] = "Programming is like building a multilingual puzzle";
+char name[152] = "Programming is like building a multilingual puzzle";
 puts(name);
-return ();
+return (0);
 }
