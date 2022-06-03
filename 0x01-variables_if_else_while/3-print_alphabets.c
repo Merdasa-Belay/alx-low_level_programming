6e4360bb4ff8b13f05981cabb7dcec0dab4bada2
Merdasa-Belay <merdasabelay@gmail.com>
2022-06-03 23:25:09 +0300

    aplhABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - This program will print english aplphabet
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
+for (ch = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
