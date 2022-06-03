3d5e26828a74b75110168822ed7c05d3eaee1141
Merdasa-Belay <merdasabelay@gmail.com>
2022-06-04 00:15:12 +0300

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - This program will print english aplphabet
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int i;
+for (i = 0;i < 10; i++)
+{
+printf("%d",i);
+}
+return (0);
+}
