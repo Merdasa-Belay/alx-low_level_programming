ade31f4d932fab9b04a37da4cce19984b9b1c0e8
Merdasa-Belay <merdasabelay@gmail.com>
2022-06-03 03:52:35 +0300

    unix basical

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - prints using the function
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+system("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
