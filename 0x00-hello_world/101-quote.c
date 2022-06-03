4f7d9804534da5486ad6c12205603194f525a0aa
Merdasa-Belay <merdasabelay@gmail.com>
2022-06-03 04:01:29 +0300

    unix basical

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
 * main - prints using the function
 *
@@ -6,6 +7,6 @@
 */
 int main(void)
 {
-system("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (1);
 }
