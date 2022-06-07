dc56cf94d1c0bf1c61bdf968d739059fc6197ab4
Merdasa-Belay <merdasabelay@gmail.com>
2022-06-08 00:15:07 +0300

    10xlphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabet
+ *
+ * Return: Always 0.
+ */
+ void print_alphabet_x10(void)
+ {
+ int ch;
+int x;
+for (x = 0;x < 1;x++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
+return (0);
+}
+
