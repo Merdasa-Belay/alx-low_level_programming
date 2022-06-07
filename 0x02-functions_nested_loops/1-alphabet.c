9831e627a4e8c6c26dc5590336068841be5d758a
Merdasa-Belay <merdasabelay@gmail.com>
2022-06-07 23:50:03 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*main - This program will print putchar
+*
+*Return: Always 0 (success)
+*/
+void print_alphabet(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
+
+
+
+
+
