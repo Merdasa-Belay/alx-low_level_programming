7a386e99480eb98ba83c9f74b5cfd7ff499e0bbd
Merdasa-Belay <merdasabelay@gmail.com>
2022-06-07 22:51:44 +0300

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,15 +4,15 @@
 *
 *Return: Always 0 (success)
 */
-
 int main(void)
 {
 char text[9] = "_putchar";
 int i = 0;
-for (i = 0 ; i < 8 ; i++)
+for (i = 0; i < 8; i++)
 {
 _putchar(text[i]);
 }
 _putchar('\n');
+
 return (0);
 }
