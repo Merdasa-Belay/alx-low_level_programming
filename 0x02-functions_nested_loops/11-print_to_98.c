5f4137a19a4ec272ba5ec7c6b0135e9d516113d4
Merdasa-Belay <merdasabelay@gmail.com>
2022-06-08 14:49:22 +0300

    prints natural no

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,14 +8,18 @@ void print_to_98(int n)
 if (n >= 98)
 {
 while (n > 98)
+{
 _putchar("%d, ", n--);
-_putchar("%d\n", n);
+_putchar("\n");
+}
 }
 else
 {
 while (n < 98)
+{
 _putchar("%d, ", n++);
-_putchar("%d\n", n);
+}
+_putchar("\n");
 }
 }
 
