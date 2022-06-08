217579c1c8da910739dd90d44235403425c2fbb4
Merdasa-Belay <merdasabelay@gmail.com>
2022-06-08 03:40:44 +0300

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,15 +4,23 @@
  *
  * Return: the last digits
  */
- void times_table(void)
- {
- int i, j;
- for (i = 0;i <= 9;i++)
- {
- for (j = 0;j <= 9;j++)
- {
- _putchar("%d * %d", i, j);
- }
- _putchar("\n");
- }
- }
+void times_table(void)
+{
+int num, mult, prod;
+for (num = 0; num <= 9; num++)
+{
+_putchar('0');
+for (mult = 1; mult <= 9; mult++)
+{
+_putchar(',');
+_putchar(' ');
+prod = num * mult;
+if (prod <= 9)
+_putchar(' ');
+else
+_putchar((prod / 10) + '0');
+_putchar((prod % 10) + '0');
+}
+_putchar('\n');
+}
+}
