779b4f9c15ce45c4daa9410eabd1d1cc9b8c5dce
Merdasa-Belay <merdasabelay@gmail.com>
2022-06-10 01:58:47 +0300

    multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,5 +1,12 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * mul - check the answer of multiplication
+ * @a: First integer
+ * @b: Second integer
+ * Return: Always 0.
+ *
+ */
 int mul(int a, int b)
 {
 int z;
