ee6d1640b43b76686d20a90b1e4cea5e4d182c8f
Merdasa-Belay <merdasabelay@gmail.com>
2022-06-20 13:21:32 +0300

    sets the value of a pointer to a char.

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,16 +8,18 @@
  */
 void print_diagsums(int *a, int size)
 {
-int i, j;
-int c = 0;
-int d = 0;
+int i, sum, sum2;
+
+sum = sum2 = 0;
+
 for (i = 0; i < size; i++)
 {
-c = c + *(a + i * sizeof(int));
+sum += a[(size * i) + i];
 }
-for (j = 0; j < size; j++)
+
+for (i = 0; i < size; i++)
 {
-d = d + *(a + (size * j) + (size - j - 1));
+sum2 += a[(i * size) + size - (i + 1)];
 }
-printf("%d, %d\n", c, d);
+printf("%d, %d\n", sum, sum2);
 }
